7e631e5092a9b70c40c2bab117dbafd94a4b73b9
virjar <weijia.deng@qunar.com>
2017-06-16 22:23:26 +0800

    bug fix

--- src/main/java/com/virjar/sipsoup/function/filter/ContainsFunction.java
+++ src/main/java/com/virjar/sipsoup/function/filter/ContainsFunction.java
@@ -14,7 +14,7 @@ public class ContainsFunction implements FilterFunction {
     @Override
     public Object call(Element element, List<SyntaxNode> params) {
         Preconditions.checkArgument(params.size() >= 2, "contains need 2 params");
-        return params.get(0).calc(element).toString().contains(params.get(1).toString());
+        return params.get(0).calc(element).toString().contains(params.get(1).calc(element).toString());
     }
 
     @Override
