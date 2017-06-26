26260821d8c5d37844829faae82dfba3bfe28c95
virjar <weijia.deng@qunar.com>
2017-06-26 22:16:03 +0800

    1.3发布

--- src/test/java/com/virjar/sipsoup/AllTextTest.java
+++ src/test/java/com/virjar/sipsoup/AllTextTest.java
@@ -3,6 +3,7 @@ package com.virjar.sipsoup;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.jsoup.Jsoup;
 
 import com.virjar.dungproxy.client.httpclient.HttpInvoker;
@@ -13,9 +14,10 @@ import com.virjar.sipsoup.parse.XpathParser;
  */
 public class AllTextTest {
     public static void main(String[] args) throws IOException {
-        // String s = IOUtils.toString(AllTextTest.class.getResourceAsStream("/html难.html"));
-        List<String> strings = XpathParser.compileNoError("/css('.jcuo1')::self()/preceding-sibling::allText()")
-                .evaluateToString(Jsoup.parse(HttpInvoker.get("http://news.mydrivers.com/1/537/537851.htm")));
+         String s = IOUtils.toString(AllTextTest.class.getResourceAsStream("/html难.html"));
+        //List<String> strings = XpathParser.compileNoError("/css('.jcuo1')::self()/preceding-sibling::allText()")
+         //       .evaluateToString(Jsoup.parse(HttpInvoker.get("http://news.mydrivers.com/1/537/537851.htm")));
+        List<String> strings = XpathParser.compileNoError("//css('.after')::*/allText()").evaluateToString(Jsoup.parse(s));
         for (String str : strings) {
             System.out.println(str);
         }
