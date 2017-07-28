1b6e2e5e30ca8f252c8c46337bd8597ce788408d
virjar <weijia.deng@qunar.com>
2017-07-28 22:54:22 +0800

    update

--- src/main/java/com/virjar/sipsoup/util/XpathUtil.java
+++ src/main/java/com/virjar/sipsoup/util/XpathUtil.java
@@ -1,14 +1,5 @@
 package com.virjar.sipsoup.util;
 
-/*
- * Copyright 2014 Wang Haomiao<et.tw@163.com> Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and limitations under the
- * License.
- */
-
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.List;
@@ -27,20 +18,10 @@ import com.virjar.sipsoup.model.XpathEvaluator;
 import com.virjar.sipsoup.parse.expression.SyntaxNode;
 
 /**
- * @author: github.com/zhegexiaohuozi [seimimaster@gmail.com] Date: 14-3-15
+ * @author virjar
  */
 public class XpathUtil {
-    public static String getJMethodNameFromStr(String str) {
-        if (str.contains("-")) {
-            String[] pies = str.split("-");
-            StringBuilder sb = new StringBuilder(pies[0]);
-            for (int i = 1; i < pies.length; i++) {
-                sb.append(pies[i].substring(0, 1).toUpperCase()).append(pies[i].substring(1));
-            }
-            return sb.toString();
-        }
-        return str;
-    }
+
 
     /**
      * 获取同名元素在同胞中的index
@@ -51,13 +32,12 @@ public class XpathUtil {
     public static int getElIndexInSameTags(Element e) {
         Elements chs = e.parent().children();
         int index = 1;
-        for (int i = 0; i < chs.size(); i++) {
-            Element cur = chs.get(i);
+        for (Element cur : chs) {
             if (e.tagName().equals(cur.tagName())) {
                 if (e.equals(cur)) {
-                    break;
+                    return index;
                 } else {
-                    index += 1;
+                    index ++;
                 }
             }
         }
